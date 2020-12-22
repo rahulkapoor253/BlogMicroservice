c245d8452b9ef9f9314bc31c409e7b6a6c4a6aea
Rahul Kapoor <rahul253801@gmail.com>
2020-12-22 20:22:02 +0530

    added sample get/fetch posts and comments client logic

---
+++ client/client/src/CommentCreate.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    //console.log(content);
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New comment</label>
+          <input
+            className="form-control"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+
+        <button style={{ marginTop: "8px" }} className="btn btn-success">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
---
+++ client/client/src/PostCreate.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default () => {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    //post axios call
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+
+        <button style={{ marginTop: "8px" }} className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
